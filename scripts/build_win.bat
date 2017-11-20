ab15bb285edcff4ad1290069cee9470fb8ea258a
pushist1y <mister.p.sh@gmail.com>
2017-11-20 21:08:33 +0300

    Refactored dependencies and updated windows build scripts

--- scripts/build_win.bat
+++ scripts/build_win.bat
@@ -3,4 +3,6 @@ cd ../src/api/DiaryScraperCore
 rd bin /s /q
 dotnet publish -r win7-x64 --output bin/dist/win
 cd ../../app 
+npm run tsc -- main.ts
+npm run ng -- build --prod
 npm run dist
--- src/app/asrc/app/diaryinput.component.ts
+++ src/app/asrc/app/diaryinput.component.ts
@@ -86,23 +86,3 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 
 
 
-@Directive({
-  selector: '[appForbiddenName]',
-  providers: [{ provide: NG_VALIDATORS, useExisting: ForbiddenValidatorDirective, multi: true }]
-})
-export class ForbiddenValidatorDirective implements Validator {
-  @Input() forbiddenName: string;
-
-  validate(control: AbstractControl): { [key: string]: any } {
-    return this.forbiddenName ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
-      : null;
-  }
-}
-
-/** A hero's name can't match the given regular expression */
-export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } => {
-    const forbidden = nameRe.test(control.value);
-    return forbidden ? { 'forbiddenName': { value: control.value } } : null;
-  };
-}
