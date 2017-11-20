0597826b063d1802736140514e01b2c836abe2e9
pushist1y <mister.p.sh@gmail.com>
2017-11-20 22:33:44 +0300

    Updated linux build script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -4,4 +4,6 @@ cd ../src/api/DiaryScraperCore
 rm -rf ./bin 
 dotnet publish -r ubuntu.16.10-x64 --output bin/dist/linux
 cd ../../app 
+npm run tsc -- main.ts
+npm run ng -- build --prod
 npm run dist
